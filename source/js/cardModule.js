be8a917d397d29cfa4989e6ad0cc7df786b6d6b4
Vlad <capkerby@gmail.com>
2021-01-26 13:42:56 +0300

    26.01.2021   a little bit of styling   started to refactor inserting rooms

--- source/js/cardModule.js
+++ source/js/cardModule.js
@@ -1,5 +1,7 @@
 'use strict';
 
+//:TODO Change structure of Facs and Caths rooms in .py
+
 (function () {
   const FLOOR_TEMPLATE = document.querySelector(`#floorTempl`).content.querySelector(`.floor`);
   const CATHEDRA_TEMPLATE = document.querySelector(`#cathedraTempl`).content.querySelector(`.cathedra`);
@@ -23,7 +25,6 @@
 
       this.popupObject.classList.remove(`popup--hidden`);
       this.locatePopup();
-
       this.listenClosePopup();
     },
 
@@ -31,32 +32,33 @@
       this.buildingRect = this.buildingObject.getBoundingClientRect();
       this.buildingData = window.dataModule.campuses[this.buildingObject.dataset.number];
       console.log(this.buildingData);
+      this.popupObject.querySelector(`.js-popup-extra`).classList.add("visually-hidden");
       FLOOR_CONTAINER.innerHTML = ``;
       CATHEDRA_CONTAINER.innerHTML = ``;
       FACS_CONTAINER.innerHTML = ``;
     },
 
     locatePopup() {
-      let posX, posY;
+      let posX = this.buildingRect.left + this.buildingRect.width, posY = this.buildingRect.top + this.buildingRect.height / 3 * 2;
       if (this.buildingRect.left + this.buildingRect.width > window.document.documentElement.clientWidth / 3 * 2) {
         posX = this.buildingRect.left + this.buildingRect.width - window.document.documentElement.clientWidth / 3;
-      } else {
-        posX = this.buildingRect.left + this.buildingRect.width;
       }
-      if (this.buildingRect.top + this.buildingRect.height / 3 * 2 + this.popupObject.clientHeight > document.documentElement.clientHeight) {
-        posY = this.buildingRect.top + this.buildingRect.height / 3 * 2 - this.popupObject.clientHeight;
-      } else {
-        posY = this.buildingRect.top + this.buildingRect.height / 3 * 2;
+      if (this.buildingRect.top + this.buildingRect.height /3 * 2 + this.popupObject.getBoundingClientRect().height > document.documentElement.clientHeight) {
+        posY =this.buildingRect.top + this.buildingRect.height /3 * 2 + this.popupObject.getBoundingClientRect().height - document.documentElement.clientHeight;
       }
-
       this.popupObject.setAttribute(`style`, `left: ${posX}px; top: ${posY}px`);
-
     },
 
     fillBuildingStructure() {
-      this.popupObject.querySelector(`.info__name-number`).textContent = `${this.buildingData.name}`;
       let purpose = String(this.buildingData.purpose)[0].toUpperCase()+String(this.buildingData.purpose).slice(1);
+
+      this.popupObject.querySelector(`.info__name-number`).textContent = `${this.buildingData.name}`;
       this.popupObject.querySelector(`.js-popup-purpose`).textContent = purpose;
+      if(this.buildingData.extra){
+        this.popupObject.querySelector(`.js-popup-extra`).classList.remove("visually-hidden");
+        this.popupObject.querySelector(`.js-popup-extra`).textContent = this.buildingData.extra;
+      }
+
       this.fillFloorTemplate();
       this.fillCathedraTemplate();
       this.fillFacsTemplate();
@@ -72,15 +74,20 @@
       Object.keys(this.buildingData.flrs).forEach( (floor, i)=> {
         let floorFragment = FLOOR_TEMPLATE.cloneNode(true);
         floorFragment.querySelector(`.floor-title`).textContent = `${window.utilModule.getSimpleTranslate(i)} Этаж`;
-        this.buildingData.flrs[floor].forEach((room)=> {
-          let roomFragment = document.createElement(`li`);
-          roomFragment.className = `structure__rooms-list-item`;
-          roomFragment.textContent = Object.keys(room)[0];
-          roomFragment.setAttribute(`title`,`${room[Object.keys(room)[0]]}`);
-          floorFragment.querySelector(`.structure__rooms-list`).appendChild(roomFragment);
-        })
+        floorFragment = this.insertRoomsIntoContainer(this.buildingData.flrs[floor], floorFragment);
         FLOOR_CONTAINER.insertAdjacentHTML(`beforeend`, floorFragment.outerHTML);
       })
+    },
+
+    insertRoomsIntoContainer(data,container){
+      data.forEach((room)=> {
+        let roomFragment = document.createElement(`li`);
+        roomFragment.className = `info__rooms-list-item`;
+        roomFragment.textContent = Object.keys(room)[0];
+        roomFragment.setAttribute(`title`,`${room[Object.keys(room)[0]]}`);
+        container.querySelector(`.info__rooms-list`).appendChild(roomFragment);
+      });
+      return container;
     },
 
 
@@ -95,9 +102,9 @@
         cathedraFragment.querySelector(`.cathedra-title`).textContent = cathedra;
         this.buildingData.cath[cathedra].forEach((room) => {
           let roomFragment = document.createElement(`li`);
-          roomFragment.className = `structure__rooms-list-item`;
+          roomFragment.className = `info__rooms-list-item`;
           roomFragment.textContent = `${room}`;
-          cathedraFragment.querySelector(`.structure__rooms-list`).appendChild(roomFragment);
+          cathedraFragment.querySelector(`.info__rooms-list`).appendChild(roomFragment);
         })
         CATHEDRA_CONTAINER.insertAdjacentHTML(`beforeend`, cathedraFragment.outerHTML);
       })
@@ -115,9 +122,9 @@
         facFragment.querySelector(`.fac-title`).textContent = fac;
         this.buildingData.facs[fac].forEach((room) => {
           let roomFragment = document.createElement(`li`);
-          roomFragment.className = `structure__rooms-list-item`;
+          roomFragment.className = `info__rooms-list-item`;
           roomFragment.textContent = `${room}`;
-          facFragment.querySelector(`.structure__rooms-list`).appendChild(roomFragment);
+          facFragment.querySelector(`.info__rooms-list`).appendChild(roomFragment);
         })
         FACS_CONTAINER.insertAdjacentHTML(`beforeend`, facFragment.outerHTML);
       })
